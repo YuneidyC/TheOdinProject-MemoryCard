4d2a2b93b6060930b86077e204189437132b368a
YuneidyC <yuneidycastillo25@gmail.com>
2025-09-10 15:49:38 +0100

    Add text description

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,7 @@ function App() {
     return (
         <>
             <h1>Memory Card</h1>
+            <p>Get points by clicking on an image but don't click on any more than once!</p>
             <div className='score-container'>
                 <table>
                     <tbody>
